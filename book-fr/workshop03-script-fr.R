50a933646ba5d453f776d7a707e46388f2f9f8d4
linley-sherin <67920340+linley-sherin@users.noreply.github.com>
2021-09-01 19:18:33 +0000

    Deploying to gh-pages from @ QCBSRworkshops/workshop03@83930b28d4b636a353474a5fb7383fdaaf256fef 🚀

---
+++ book-fr/workshop03-script-fr.R
@@ -0,0 +1,36 @@
+##Section: 01-intro.R 
+
+par(mar = c(4, 4, .1, .1))
+plot(pressure, type = 'b', pch = 19)
+
+knitr::kable(
+  head(iris, 20), caption = 'Here is a nice table!',
+  booktabs = TRUE
+)
+
+
+##Section: 02-literature.R 
+
+
+
+
+##Section: 03-method.R 
+
+
+
+
+##Section: 04-application.R 
+
+
+
+
+##Section: 05-summary.R 
+
+
+
+
+##Section: 06-references.R 
+
+
+
+
