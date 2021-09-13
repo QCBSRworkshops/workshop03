6a2bc169fdd803a44ccb711a869703204dbf643e
katherinehebert <katherine.ahebert@gmail.com>
2021-09-13 17:40:57 +0000

    Deploying to gh-pages from @ QCBSRworkshops/workshop03@8ebf4522daba9f25b3acaef6340c375015539d57 🚀

--- book-fr/workshop03-script-fr.R
+++ book-fr/workshop03-script-fr.R
@@ -1,6 +1,6 @@
 ##Section: 01-introduction.R 
 
-# Install the required packages
+# Installez les librairies requises
 install.packages("gridExtra")
 install.packages("ggplot2")
 install.packages("ggsignif")
@@ -11,8 +11,22 @@ install.packages("RColorBrewer")
 install.packages("reshape2")
 install.packages("GGally")
 install.packages("patchwork")
-install.packages("plotly")
+#install.packages("plotly")
 install.packages("palmerpenguins")
+
+# Load the required packages
+library(gridExtra)
+library(ggplot2)
+library(ggsignif)
+library(ggdendro)
+library(maps)
+library(mapproj)
+library(RColorBrewer)
+library(reshape2)
+library(GGally)
+library(patchwork)
+#library(plotly)
+library(palmerpenguins)
 
 
 ##Section: 02-visualization-en-science.R 
